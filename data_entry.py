6ee1b0e8a4a93bbc983cdd9e37996213a1e713f6
nahom zenebe <nahomzenebe123@gmail.com>
2025-02-12 15:33:33 +0300

    Add data to  data entry script and finance data

--- data_entry.py
+++ data_entry.py
@@ -1,2 +1,21 @@
+from datetime import datetime
 
+def get_date(prompt,allow_default=False):
+    date_str = input(prompt)
+    if allow_default and not date_str:
+        return  datetime.today().strftime('%d-%m-%Y')
+    try:
+        valid_date=datetime.strptime(date_str,'%d-%m-%Y')
+        return valid_date.strftime('%d-%m-%Y')
+    except ValueError:
+        print('Please enter a valid date in this format dd-mm-yy')
+        return get_date(prompt,allow_default)
+def get_amount():
+    pass
+
+def get_category():
+    pass
+
+def get_description():
+    pass
 
